1c1fee373655f97e29cb8bfee8121c028a1d42ba
himanshuthecoder <himanshusharma2972@gmail.com>
2020-08-02 14:24:26 +0530

    username updated

--- random_bouncing_Ball.java
+++ random_bouncing_Ball.java
@@ -3,7 +3,7 @@
 *
 *           Himanshu Sharma
 *           himanshusharma2972@gmail.com
-*           Github - himanshu-sharma2018
+*           Github - himanshuthecoder
 *		********************************************
 *                 Bouncing Ball Pattern 2
 *         ********************************************
